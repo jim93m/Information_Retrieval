530ec09a60c0fda0aa8013d14f77093feec34a83
jim <jim93m@gmail.com>
2020-06-17 00:57:08 +0300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Information_Retrieval',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Jim',
+    author_email='jim93m@gmail.com',
+    description=''
+)
